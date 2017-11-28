9912344a235b9ba2d9d13ac706ce7fb05121fa68
RodrigoEzequiel <33558288+RodrigoEzequiel@users.noreply.github.com>
2017-11-28 17:38:13 -0300

    Update juego.js

--- js/juego.js
+++ js/juego.js
@@ -1,18 +1,14 @@
-// Representación de la grilla. Cada nro representa a una pieza.
-// El 9 es la posición vacía
 var grilla = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9]
 ];
 
-// Ac&aacute; vamos a ir guardando la posición vacía
 var posicionVacia = {
   fila:2,
   columna:2
 };
 
-// Esta función va a chequear si el Rompecabezas est&aacute; en la posición ganadora
 function chequearSiGano(){
   if (grilla[0][0]==1 && grilla[0][1]==2 &&grilla[0][2]==3 &&
       grilla[1][0]==4 && grilla[1][1]==5 &&grilla[1][2]==6 && 
@@ -24,14 +20,10 @@ function chequearSiGano(){
     }
 }
 
-
-// la hacen los alumnos, pueden mostrar el cartel como prefieran. Pero es importante que usen
-// esta función
 function mostrarCartelGanador(){
     alert("ganaste!!")
 }
 
-// Intercambia posiciones grilla y en el DOM
 function intercambiarPosiciones(fila1, columna1, fila2, columna2){
   var Imagen =  document.getElementById("img"+grilla[fila2][columna2]);
   var Vacio =  document.getElementById("img"+grilla[fila1][columna1]);
@@ -48,14 +40,12 @@ function intercambiarPosiciones(fila1, columna1, fila2, columna2){
   grilla[fila2][columna2] = 9; 
 }
 
-// Actualiza la posición de la pieza vacía
 function actualizarPosicionVacia(nuevaFila,nuevaColumna){
   posicionVacia.fila = nuevaFila;
   posicionVacia.columna = nuevaColumna;
 }
 
 
-// Para chequear si la posicón está dentro de la grilla.
 function posicionValida(fila,columna){
   if (fila<=2 && fila>=0 && columna>=0 && columna<=2){
     return true;
@@ -65,32 +55,30 @@ function posicionValida(fila,columna){
     }
 }
 
-// Movimiento de fichas, en este caso la que se mueve es la blanca intercambiando
-// su posición con otro elemento
 function moverEnDireccion(direccion){
 
   var nuevaFilaPiezaVacia;
   var nuevaColumnaPiezaVacia;
 
-  // Intercambia pieza blanca con la pieza que está arriba suyo
+
   if(direccion == 40){
     nuevaFilaPiezaVacia = posicionVacia.fila-1;
     nuevaColumnaPiezaVacia = posicionVacia.columna;
   }
-  // Intercambia pieza blanca con la pieza que está abajo suyo
+
   else if (direccion == 38) {
     nuevaFilaPiezaVacia = posicionVacia.fila+1;
     nuevaColumnaPiezaVacia = posicionVacia.columna;
 
   }
-  // Intercambia pieza blanca con la pieza que está a su izq
+
   else if (direccion == 39) {
     // Completar
     nuevaFilaPiezaVacia = posicionVacia.fila;
     nuevaColumnaPiezaVacia = posicionVacia.columna-1;
 
   }
-  // Intercambia pieza blanca con la pieza que está a su der
+
   else if (direccion == 37) {
     // Completar
     nuevaFilaPiezaVacia = posicionVacia.fila;
@@ -106,9 +94,6 @@ function moverEnDireccion(direccion){
 
 }
 
-
-
-// Extras, ya vienen dadas
 
 function mezclarPiezas(veces){
   if(veces<=0){return;}
@@ -144,3 +129,9 @@ function iniciar(){
 
 
 iniciar();
+
+///////////////////////////////////////
+////Rodrigo Ezequiel Delascio  ♥  /////
+///rodrigo_ezequiel666@hotmail.com/////
+//https://github.com/RodrigoEzequiel///
+///////////////////////////////////////
